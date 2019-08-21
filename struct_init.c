fb72c406bb42051c2f2308540c80304b3ec528d1
Jason mallister Ormond yronwood <jormond-@at-d4.21-school.ru>
2019-08-21 18:15:29 +0300

    parser begin

--- struct_init.c
+++ struct_init.c
@@ -6,7 +6,7 @@
 /*   By: jormond- <jormond-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/21 16:13:37 by jormond-          #+#    #+#             */
-/*   Updated: 2019/08/21 18:09:03 by jormond-         ###   ########.fr       */
+/*   Updated: 2019/08/21 18:15:06 by jormond-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,8 @@
 void    struct_init(t_printf *p)
 {
     ft_bzero(p->flags, 5);
-	printf("c");
+	p->width = 0;
+	p->prec = 0;
+	ft_bzero(p->modifier, 4);
+	p->specifier = '\0';
 }
